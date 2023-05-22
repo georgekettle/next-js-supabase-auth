6100752f771f3c8ed5a5ecd2bca8fe266511deed
george kettle <georgekettle.gk@gmail.com>
2023-05-22 15:44:22 +0200

    Home page with search bar (not functional)

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
+import SearchBar from '@/components/SearchBar'
+
 export default function Home() {
   return (
     <main>
-      Hello
+      <div class="bg-gradient-to-r from-info-100 to-primary-600 text-white py-24">
+        <div class="px-4 space-y-8">
+          <div class="text-center space-y-4 max-w-lg mx-auto">
+            <h1 class="tracking-tight text-4xl">Get More Done in Less Time</h1>
+            <p class="text-normal mb-4 text-white/80">Elevate your business with custom chatbots. Enjoy increased profitability and efficiency with pre-trained solutions.</p>
+          </div>
+          <div class="max-w-md mx-auto">
+            <SearchBar />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,11 +9,38 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        primary: {
+          '50': '#fbf5ff',
+          '100': '#f3e0ff',
+          '200': '#e9c7ff',
+          '300': '#d89eff',
+          '400': '#c261ff',
+          '500': '#ad29ff',
+          '600': '#9f0fff',
+          '700': '#7b0cc0',
+          '800': '#5e0e90',
+          '900': '#470c6a',
+          '950': '#26003d',
+        },
+        secondary: {
+          '50': '#fffaeb',
+          '100': '#fff4cc',
+          '200': '#ffe799',
+          '300': '#ffd35c',
+          '400': '#ffb41f',
+          '500': '#f09800',
+          '600': '#e07b00',
+          '700': '#a85400',
+          '800': '#853c00',
+          '900': '#612d00',
+          '950': '#1a0b00',
+        },
+        tertiary: colors.neutral,
+        success: colors.lime,
+        danger: colors.red,
+        info: colors.amber,
+      }
     },
   },
   plugins: [],
