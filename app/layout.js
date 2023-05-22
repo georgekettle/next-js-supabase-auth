0b1a605ec14011a0e740b3bd01344378be71c11a
george kettle <georgekettle.gk@gmail.com>
2023-05-22 18:38:59 +0200

    Configured supabase provider component for auth in client side

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,36 @@
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { headers, cookies } from 'next/headers'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 import Navbar from '@/components/Navbar'
+import SupabaseProvider from './SupabaseProvider'
 
 export const metadata = {
   title: 'Chatterbot',
   description: 'Create and monetise your own chatbot',
 }
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const supabase = createServerComponentSupabaseClient({
+    headers,
+    cookies,
+  })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <SupabaseProvider session={session}>
+          <Navbar />
+          {children}
+        </SupabaseProvider>
       </body>
     </html>
   )
